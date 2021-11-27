8b8177b50292113fe515583f305be146ecbcb6a8
suanfa8 <45396320+suanfa8@users.noreply.github.com>
2021-11-27 17:56:15 +0800

    添加了 Consul 客户端配置。

---
+++ consulclient8889/src/main/java/com/suanfa8/Consulclient8889Application.java
@@ -0,0 +1,13 @@
+package com.suanfa8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consulclient8889Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Consulclient8889Application.class, args);
+    }
+
+}
