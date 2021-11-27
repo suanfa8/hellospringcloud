07c4c060eec7d954ec2e7d01aa947fc2c466ae39
suanfa8 <45396320+suanfa8@users.noreply.github.com>
2021-11-27 18:34:19 +0800

    在商品服务里，添加了一个对外的服务。

--- products9998/src/main/java/com/suanfa8/controller/ProductController.java
+++ products9998/src/main/java/com/suanfa8/controller/ProductController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @Slf4j
 public class ProductController {
@@ -14,8 +17,20 @@ public class ProductController {
 
     // http://localhost:9998/product/showMsg
     @GetMapping("/product/showMsg")
-    public String showMsg(){
+    public String showMsg() {
         log.info("进入商品服务，展示商品");
         return "进入商品服务，展示商品 " + port;
     }
+
+    // http://localhost:9998/product/findAll
+    @GetMapping("/product/findAll")
+    public Map<String, Object> findAll() {
+        log.info("商品服务查询所有调用成功,当前服务端口:[{}]", port);
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("msg", "服务调用成功,服务提供端口为: " + port);
+        map.put("status", true);
+        return map;
+    }
+
+
 }
