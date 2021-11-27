c6b8739c76e8fa311f13dd388598cf063046b965
suanfa8 <45396320+suanfa8@users.noreply.github.com>
2021-11-28 01:35:31 +0800

    添加了 Config 组件的服务端。

---
+++ configserver7878/src/test/java/com/suanfa8/Configserver7878ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suanfa8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Configserver7878ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
