040bf4cdb49c68d685357cddb2274e31e2966c0f
suanfa8 <45396320+suanfa8@users.noreply.github.com>
2021-11-27 18:30:12 +0800

    在「用户服务」使用 restTemplate 调用「商品服务」。

---
+++ users9999/src/main/java/com/suanfa8/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.suanfa8.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+public class UserController {
+
+    // http://localhost:9999/user/findAll
+    @GetMapping("/user/findAll")
+    public String findAll() {
+        log.info("调用用户服务...");
+        // 1. 使用 restTemplate 调用商品服务
+        RestTemplate restTemplate = new RestTemplate();
+        // 参数 1：请求路径
+        // 参数 2：返回值类型
+        String forObject = restTemplate.getForObject("http://localhost:9998/product/showMsg", String.class);
+        return forObject;
+    }
+}
