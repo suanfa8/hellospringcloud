d97283ebbf7c6ba103b66dec3cffb891693fd8b2
suanfa8 <45396320+suanfa8@users.noreply.github.com>
2021-11-27 17:18:13 +0800

    添加了 Eureka 的一个客户端。

---
+++ eurekaclient8888/src/main/java/com/suanfa8/Eurekaclient8888Application.java
@@ -0,0 +1,15 @@
+package com.suanfa8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Eurekaclient8888Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eurekaclient8888Application.class, args);
+    }
+
+}
